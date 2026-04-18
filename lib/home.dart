50a163b70c05257529aed44dcac2d2c3fd719af0
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2026-04-19 00:02:43 +0500

    "Integrating API"

--- lib/home.dart
+++ lib/home.dart
@@ -58,11 +58,7 @@ class _HomeState extends State<Home> {
                               "") {
                             print("Blank search");
                           } else {
-                            Navigator.pushReplacementNamed(
-                              context,
-                              "/loading",
-                              arguments: {"searchText": searchController.text},
-                            );
+                            getRecipe(searchController.text);
                           }
                         },
                         child: Container(
