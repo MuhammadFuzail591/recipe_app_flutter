05e82c645d1b41f197c427cffa29d123c5f4c225
Muhammad Fuzail <muhammadfuzail591@gmail.com>
2026-04-25 15:00:18 +0500

    Recipe details model done, now working on class

--- lib/recipe_details.dart
+++ lib/recipe_details.dart
@@ -1,8 +1,93 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:recipe_app/models/recipe_details_model.dart';
 
-class RecipeDetailsPage extends StatelessWidget{
+class RecipeDetailsPage extends StatefulWidget {
+  final String mealId;
 
-  final Meal meal;
-  const RecipeDetailPage({super.key, required this.meal})
+  const RecipeDetailsPage({required this.mealId, super.key});
 
+  @override
+  State<RecipeDetailsPage> createState() => _RecipeDetailsPageState();
+}
+
+class _RecipeDetailsPageState extends State<RecipeDetailsPage> {
+  RecipeDetailsModel? meal;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMeal();
+  }
+
+  Future<void> fetchMeal() async {
+    final url =
+        "https://www.themealdb.com/api/json/v1/1/lookup.php?i=${widget.mealId}";
+
+    final response = await get(Uri.parse(url));
+    final data = jsonDecode(response.body);
+    final mealData = data["meals"][0];
+
+    setState(() {
+      meal = RecipeDetailsModel.fromMap(mealData);
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isLoading) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF0D1B2A),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildHeader(),
+            // _buildTitleSection(),
+            // _buildIngredients(),
+            // _buildInstructions(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+          child: Image.network(
+            meal.strMealThumb,
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        // Back button
+        Positioned(
+          top: 40,
+          left: 16,
+          child: CircleAvatar(
+            backgroundColor: Colors.black54,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
